baaff52e4f004912b94335f11243fde155434a9c
vasiliy_grinev <vass_stn@mail.ru>
2025-07-21 10:38:31 +0300

    add view docs

--- url-shortener/main.py
+++ url-shortener/main.py
@@ -1,3 +1,21 @@
-from fastapi import FastAPI
+from fastapi import (
+    FastAPI,
+    Request,
+)
 
 app = FastAPI(title="URL Shortener")
+
+
+@app.get("/")
+def read_root(
+    requests: Request,
+    name: str = "World",
+):
+    docs_url = requests.url.replace(
+        path="/docs",
+        query="",
+    )
+    return {
+        "message": f"Hello {name}!",
+        "docs": str(docs_url),
+    }
